15b062d76178920b38ab97df6f719deed9fbff36
gene <iamgenechua@gmail.com>
2020-07-31 01:46:11 +0800

    Firestone security rules

--- ionic-daily-moments/src/pages/FirstEntry.tsx
+++ ionic-daily-moments/src/pages/FirstEntry.tsx
@@ -2,21 +2,24 @@ import React, { useEffect, useState } from 'react';
 import {IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButtons, IonBackButton} from '@ionic/react';
 import { useParams } from 'react-router';
 import {firestore} from '../Firebase';
+import {Entry, toEntry} from '../models';
+import { useAuth } from '../auth';
 
 interface RouteParams {
     id: string;
 }
 
 const FirstEntry: React.FC = () => {
+    const {userId} = useAuth();
     const {id} = useParams<RouteParams>();
-    const [entry, setEntry] = useState<any>();
+    const [entry, setEntry] = useState<Entry>();
     useEffect(() => {
-        const entryRef = firestore.collection('todos').doc(id);
-        entryRef.get().then((doc) => {
-            const entry = {id: doc.id, ...doc.data()};
-            setEntry(entry);
-        })
-    }, [id]);
+        const entryRef = firestore
+            .collection('users')
+            .doc(userId)
+            .collection('todos').doc(id);
+        entryRef.get().then((doc) => setEntry(toEntry(doc)));
+    }, [userId, id]);
 
     return (
         <IonPage>
--- ionic-daily-moments/src/pages/Home.tsx
+++ ionic-daily-moments/src/pages/Home.tsx
@@ -1,19 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import {IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonItem, IonList} from '@ionic/react';
 import {firestore} from '../Firebase';
+import {Entry, toEntry} from '../models';
+import { useAuth } from '../auth';
+
 
 const Home: React.FC = () => {
-  const [entries, setEntries] = useState([]);
+  const {userId} = useAuth();
+  const [entries, setEntries] = useState<Entry[]>([]);
   useEffect(() => {
-    const todosRef = firestore.collection('todos');
+    const todosRef = firestore
+      .collection('users')
+      .doc(userId)
+      .collection('todos');
     todosRef.get().then((snapshot) => {
-      const todos = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data()
-      }));
+      const todos = snapshot.docs.map((doc) => toEntry(doc));
     setEntries(todos);
     });
-  }, []); // empty array means it will only run once when the component is mounted
+  }, [userId]); // empty array means it will only run once when the component is mounted
   return (
   <IonPage>
     <IonHeader>
