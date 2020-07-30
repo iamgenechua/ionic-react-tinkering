83e05885b991a099e5dc1b85b110613bb1e77200
gene <iamgenechua@gmail.com>
2020-07-30 17:28:22 +0800

    Logging in and out of firebase

--- ionic-daily-moments/src/App.tsx
+++ ionic-daily-moments/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { IonApp } from '@ionic/react';
+import { IonApp, IonLoading } from '@ionic/react';
 import {IonReactRouter} from '@ionic/react-router';
 import AppPrivate from './AppPrivate';
 import { Route, Switch, Redirect} from 'react-router-dom';
@@ -11,18 +11,24 @@ import {auth} from './Firebase';
 
 
 const App: React.FC = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
+
+  //logging in function
+  const [authState, setAuthState] = useState({loading: true, loggedIn: false});
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
-      setLoggedIn(Boolean(user)); // when null, become false, else, true
+      setAuthState({loading: false, loggedIn: Boolean(user)}); // when null, become false, else, true
     })
   }, []);
 
-  console.log(`Rendering App with LoggedIn=${loggedIn}`);
+  console.log(`Rendering App with LoggedIn=${authState}`);
+
+  if (authState.loading) {
+    return <IonLoading isOpen />;
+  }
 
   return (
     <IonApp>
-      <AuthContext.Provider value={{loggedIn}}>
+      <AuthContext.Provider value={{loggedIn: authState.loggedIn}}>
         <IonReactRouter>
             <Switch>
                 <Route exact path="/login">
--- ionic-daily-moments/src/pages/Settings.tsx
+++ ionic-daily-moments/src/pages/Settings.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {IonHeader, IonToolbar, IonTitle, IonContent, IonPage} from '@ionic/react';
+import {IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButton} from '@ionic/react';
+import {auth} from '../Firebase';
 
 const Settings: React.FC = () => (
   <IonPage>
@@ -9,7 +10,11 @@ const Settings: React.FC = () => (
       </IonToolbar>
     </IonHeader>
     <IonContent className="ion-padding">
-      Settings Page
+      {/* Log out function using Firebase API */}
+      <IonButton 
+        color="medium" 
+        expand="block"
+        onClick={() => auth.signOut()}>Logout</IonButton>
     </IonContent>
   </IonPage>
 );
